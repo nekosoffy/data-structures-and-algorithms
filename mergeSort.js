355b851d0a5b90051e2685b7844e2534eb0171fc
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-10-07 14:09:54 -0300

    Create recursive Merge Sort function

---
+++ mergeSort.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+function mergeSort(array) {
+  if (array.length <= 1) return array;
+  let left = array.slice(0, array.length / 2);
+  let right = array.slice(array.length / 2);
+
+  if (left.length > 1) {
+    left = mergeSort(left);
+  }
+
+  if (right.length > 1) {
+    right = mergeSort(right);
+  }
+
+  function merge(left, right) {
+    const sortedArray = [];
+
+    if (left.length > 0 && right.length > 0) {
+      if (left[0] < right[0]) {
+        sortedArray.push(left.shift());
+      } else {
+        sortedArray.push(right.shift());
+      }
+      return sortedArray.concat(merge(left, right));
+    }
+
+    if (left.length === 0) return sortedArray.concat(right);
+    if (right.length === 0) return sortedArray.concat(left);
+  }
+
+  return merge(left, right);
+}
+
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
+console.log(mergeSort([105, 79, 100, 110]));
