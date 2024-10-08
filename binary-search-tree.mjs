2af9a791212072da4439c4807f34be60b53b4990
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-10-08 18:56:08 -0300

    Refactor code to make it more clean

--- binary-search-tree.mjs
+++ binary-search-tree.mjs
@@ -1,52 +1,59 @@
 #!/usr/bin/env node
 import mergeSort from './mergeSort.mjs';
 
-function node(data = null) {
-  return { data, leftChild: null, rightChild: null };
-}
-
-function buildTree(array) {
-  if (!Array.isArray(array) || array.length === 0) {
-    throw new TypeError('Invalid input.');
-  }
-
-  array = [...new Set(array)];
-  array = mergeSort(array);
-
-  if (array.length === 1) {
-    return node(array[0]);
-  }
-
-  const mid = Math.floor(array.length / 2);
-  let rootNode = node(array[mid]);
-
-  if (array.length > 2) {
-    const left = array.slice(0, mid);
-    const right = array.slice(mid + 1);
-    rootNode.leftChild = buildTree(left);
-    rootNode.rightChild = buildTree(right);
-  } else {
-    const right = [array[1]];
-    rootNode = node(array[0]);
-    rootNode.rightChild = buildTree(right);
-  }
-
-  return rootNode;
-}
-
 function tree(array) {
-  try {
-    const root = buildTree(array);
-    return root;
-  } catch (e) {
-    console.error(e);
+  function node(data = null) {
+    return { data, leftChild: null, rightChild: null };
   }
+
+  function buildTree(array) {
+    try {
+      if (!Array.isArray(array) || array.length === 0) {
+        throw new TypeError('Input must be a non-empty array.');
+      }
+
+      array = [...new Set(array)];
+      array = mergeSort(array);
+
+      if (array.length === 1) {
+        return node(array[0]);
+      }
+
+      const mid = Math.floor(array.length / 2);
+      let rootNode = node(array[mid]);
+
+      if (array.length > 2) {
+        const left = array.slice(0, mid);
+        const right = array.slice(mid + 1);
+        rootNode.leftChild = buildTree(left);
+        rootNode.rightChild = buildTree(right);
+      } else {
+        const right = [array[1]];
+        rootNode = node(array[0]);
+        rootNode.rightChild = buildTree(right);
+      }
+
+      return rootNode;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  const firstNode = buildTree(array);
+
+  function root() {
+    return firstNode;
+  }
+
+  return { root, insert };
 }
 
-const result = tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+const binarySearchTree = tree([
+  1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324,
+]);
 
 const prettyPrint = (node, prefix = '', isLeft = true) => {
-  if (node === null) {
+  if (node == null) {
     return;
   }
   if (node.rightChild !== null) {
@@ -58,4 +65,4 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
   }
 };
 
-prettyPrint(result);
+prettyPrint(binarySearchTree.root());
